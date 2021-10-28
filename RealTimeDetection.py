c4d0fd7da0def4765aac7a552e46012ab862b956
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2021-10-28 22:30:26 +0330

    Q01 almost completed

--- RealTimeDetection.py
+++ RealTimeDetection.py
@@ -6,7 +6,7 @@ import numpy as np
 
 sess = tf.Session()
 
-new_saver = tf.train.import_meta_graph('Sign_Detector.meta')
+new_saver = tf.train.import_meta_graph('Sign_Detector_77pAcc.meta')
 new_saver.restore(sess, tf.train.latest_checkpoint('./'))
 
 alphabets =  'ABCDEFGHIKLMNOPQRSTUVWXY'
@@ -28,10 +28,14 @@ while(1):
         ROI_small = cv2.resize(ROI_gray, (28,28))
         ROI_small_flat = cv2.resize(ROI_gray, (28*28,1))
         ROI = np.array(ROI_small_flat, dtype=np.float32)
+        display = cv2.rectangle(frame.copy(),(200,100),(500,400),(0,255,0),2)
+       # probs = sess.run('Y_PRED:0', feed_dict={'X_INPUT:0':ROI})
+        #for count,i in enumerate(probs):
+         #   text = "- {0}:{1:.2f}% ".format(alphabets[count], 100*i)
+          #  cv2.putText(display, text, (200+5*i, 500), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
         label = sess.run('Y_PRED_CLS:0', feed_dict={'X_INPUT:0':ROI})
         word = alphabets[int(label)]
-        display = cv2.rectangle(frame.copy(),(200,100),(500,400),(0,255,0),2) 
-        cv2.putText(display, word, (200, 500), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+        cv2.putText(display, word, (200, 400), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
         cv2.imshow('curFrame',display)
         cv2.imshow('Roi', ROI_gray)
         
